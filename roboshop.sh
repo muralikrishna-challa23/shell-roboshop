880722c1b30c549316ed9feb4a40970f0d1e53f4
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-24 14:19:36 +0530

    roboshop shell

---
+++ roboshop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0da37bd7ac039a4cf"
+HOSTED_ZONE="Z026855721W2V0QCE5DEE"
+DOMAIN_NAME="mkreddy.shop"
+
+for instance in $@
+do
+
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output text)
+
+    if [ $instance -ne "frontend"]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PublicIpAddress' --output text)
+
+        RECORD_NAME=$instance.$DOMAIN_NAME
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PrivateIpAddress' --output text)
+
+        RECORD_NAME=$DOMAIN_NAME
+    fi
+
+    echo $instance $RECORD_NAME
+
+done
