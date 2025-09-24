b9d16b6daf7e73b9d7e035164c98b6f0ad087c7f
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-24 14:24:49 +0530

    roboshop shell

--- roboshop.sh
+++ roboshop.sh
@@ -10,7 +10,7 @@ do
 
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Test}]' --query 'Instances[0].InstanceId' --output text)
 
-    if [ $instance -ne "frontend" ]; then
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PublicIpAddress' --output text)
 
         RECORD_NAME=$instance.$DOMAIN_NAME
