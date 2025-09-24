24ca40f19dff16a7df3044f79c8b95324dbfb564
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-24 14:47:33 +0530

    roboshop shell

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ DOMAIN_NAME="mkreddy.shop"
 for instance in $@
 do
 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[].Instances[].PublicIpAddress' --output text)
@@ -24,8 +24,8 @@ do
 
     
 aws route53 change-resource-record-sets
-     --hosted-zone-id $HOSTED_ZONE
-     --change-batch '
+    --hosted-zone-id $HOSTED_ZONE
+    --change-batch '
         {
         "Comment": "Updating RECORD for ROUTE53",
         "Changes": [
