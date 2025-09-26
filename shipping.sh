3ca31c98aa1efdc1275d594ec76785a2f72fee1c
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-27 01:12:33 +0530

    roboshop shell

--- shipping.sh
+++ shipping.sh
@@ -80,8 +80,8 @@ VALIDATE $? "Starting shipping"
 
 dnf install mysql -y &>>$LOG_FILE
 
-mysql -h mysql.mkreddy.shop -uroot -pRoboShop@1 -e 'use cities'
-if [$? -ne 0]; then
+mysql -h mysql.mkreddy.shop -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
+if [ $? -ne 0 ]; then
     mysql -h mysql.mkreddy.shop -uroot -pRoboShop@1 < /app/db/schema.sql  &>>$LOG_FILE
     mysql -h mysql.mkreddy.shop -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
     mysql -h mysql.mkreddy.shop -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
