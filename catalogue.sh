cb0f77e164d6837d942719b5470f0c18e6ae7f41
muralikrishna challa <muralikrishna.challa23@gmail.com>
2025-09-26 21:02:22 +0530

    roboshop shell

--- catalogue.sh
+++ catalogue.sh
@@ -38,7 +38,7 @@ VALIDATE $? "Enable NodeJs 20"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing NodeJs"
 
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating System user roboshop"
